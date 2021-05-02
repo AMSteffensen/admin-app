0660b68a343d51df035fdc914b4720628e6207ae
steand <andreas.steffensen@n5n.no>
2021-05-03 00:21:58 +0200

    add authprovider

--- client/src/App.js
+++ client/src/App.js
@@ -5,60 +5,23 @@ import restProvider from "ra-data-simple-rest";
 import PostList from "./components/posts/PostsList";
 import PostCreate from "./components/posts/PostsCreate";
 import PostEdit from "./components/posts/PostsEdit";
+import authProvider from "./authProvider";
+
 import "./App.css";
 
 function App() {
-  const adminUser = {
-    email: "admin@a",
-    password: "admin@a",
-  };
-
-  const normalUser = {
-    email: "normal@user.com",
-    password: "user",
-  };
-  const [user, setUser] = useState({ name: "", email: "" });
-  const [error, setError] = useState("");
-
-  const Login = (details) => {
-    console.log(details);
-
-    if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
-    ) {
-      setUser({
-        name: details.name,
-        email: details.email,
-      });
-    } else {
-      setError("Details does not match");
-    }
-  };
-
-  const Logout = ({ Login, error }) => {
-    setUser({ name: "", email: "" });
-  };
-
   return (
-    <div className="App">
-      {user.email !== "" ? (
-        <Admin dataProvider={restProvider("http://localhost:3000")}>
-          <h2>
-            Welcome, <span>{user.name}</span>
-          </h2>
-          <button onClick={Logout}>Logout</button>
-          <Resource
-            name="posts"
-            list={PostList}
-            create={PostCreate}
-            edit={PostEdit}
-          ></Resource>
-        </Admin>
-      ) : (
-        <LoginForm Login={Login} error={error} />
-      )}
-    </div>
+    <Admin
+      dataProvider={restProvider("http://localhost:3000")}
+      authProvider={authProvider}
+    >
+      <Resource
+        name="posts"
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      ></Resource>
+    </Admin>
   );
 }
 
