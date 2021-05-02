4091a76af8647543eb2b8878761fe9baa13e6da7
steand <andreas.steffensen@n5n.no>
2021-05-02 23:22:35 +0200

    setup react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,53 @@
 import React, { useState } from "react";
 import LoginForm from "./components/LoginForm";
+import "./App.css";
 
 function App() {
   const adminUser = {
     email: "admin@admin.com",
     password: "admin123",
   };
+
+  const normalUser = {
+    email: "normal@user.com",
+    password: "user",
+  };
   const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
+  const [sidebarOpen, setsidebarOpen] = useState(false);
+
+  const closeSidebar = () => {
+    setsidebarOpen(false);
+  };
 
   const Login = (details) => {
     console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      setError("Details does not match");
+    }
   };
 
   const Logout = ({ Login, error }) => {
-    console.log("logout");
+    setUser({ name: "", email: "" });
   };
+
   return (
     <div className="App">
       {user.email !== "" ? (
-        <div className="welcome">
+        <div className="container">
           <h2>
             Welcome, <span>{user.name}</span>
           </h2>
-          <button>Logout</button>
+          <button onClick={Logout}>Logout</button>
         </div>
       ) : (
         <LoginForm Login={Login} error={error} />
